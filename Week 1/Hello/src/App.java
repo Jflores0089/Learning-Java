243767958d4426c9ca7ca84ed624e8353e0fab1c
Javier Flores <jflores0089@gmail.com>
2026-05-13 22:01:32 -0400

    Day 1 - Hello World and first challenge

---
+++ Week 1/Hello/src/App.java
@@ -0,0 +1,9 @@
+public class App{
+    public static void main(String[] args){
+
+       for (int i = 1; i < 6; i++){
+
+            System.out.println(String.valueOf(i).repeat(i));
+       }
+    }
+}
