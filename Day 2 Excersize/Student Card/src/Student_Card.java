1c26d3f2619f81cc18f6bb53509b66f8320c4800
Javier <jflores0089@gmail.com>
2026-05-19 00:34:22 -0400

    finished excersize 2

--- Day 2 Excersize/Student Card/src/Student_Card.java
+++ Day 2 Excersize/Student Card/src/Student_Card.java
@@ -1,24 +1,59 @@
 import java.util.Scanner;
 
-public class Student_Card{
-    
-    public static void main(String[]args){
+public class Student_Card {
+
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("What is your name?");
-        String name = scanner.next();
-        System.out.print("What is your age?");
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("What is your age?");
         int age = Integer.valueOf(scanner.nextLine());
-        System.out.print("What is your GPA?");
+
+        System.out.println("What is your major?");
+        String major = scanner.nextLine();
+
+        System.out.println("What is your GPA?");
         double gpa = Double.valueOf(scanner.nextLine());
 
+        System.out.println("What is your favorite programming language?");
+        String fav = scanner.nextLine();
 
+        System.out.println();
+        System.out.println();
 
+        for (int i = 0; i < 33; i++) {
+            System.out.print("=");
+        }
+        System.out.println();
+        System.out.print("        Student Profile");
+        System.out.println();
 
-    
+        for (int i = 0; i < 33; i++) {
 
+            System.out.print("=");
+        }
 
+        System.out.println();
 
+        System.out.printf("Name: %s\n", name);
+        System.out.printf("Age: %d\n", age);
+        System.out.printf("Major: %s\n", major);
+        System.out.printf("GPA: %.2f\n", gpa);
+        System.out.printf("Language: %s\n", fav);
+
+        for (int i = 0; i < 33; i++) {
+
+            System.out.print("=");
+        }
+
+        System.out.println();
+
+        if (gpa > 3.5) {
+            System.out.println("*** Dean's List Student ***");
+        }
 
         scanner.close();
     }
