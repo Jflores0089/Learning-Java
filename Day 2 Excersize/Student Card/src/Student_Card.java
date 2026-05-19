d33e3b63a524e4bedd2f9f7515a0b3429fdd84ca
Javier Flores <jflores0089@gmail.com>
2026-05-18 23:21:46 -0400

    Working on the excersize

--- Day 2 Excersize/Student Card/src/Student_Card.java
+++ Day 2 Excersize/Student Card/src/Student_Card.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class Student_Card{
+    
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name?");
+        String name = scanner.next();
+        System.out.print("What is your age?");
+        int age = Integer.valueOf(scanner.nextLine());
+        System.out.print("What is your GPA?");
+        double gpa = Double.valueOf(scanner.nextLine());
+
+
+
+
+    
+
+
+
+
+        scanner.close();
     }
 }
